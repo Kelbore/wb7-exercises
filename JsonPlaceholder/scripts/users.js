373fc8a8812d91e49352fd0b14695bddc7516e01
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-06-06 17:41:36 -0400

    modify users.js file

--- JsonPlaceholder/scripts/users.js
+++ JsonPlaceholder/scripts/users.js
@@ -7,19 +7,20 @@ const usersInput = document.getElementById('usersInput');
 
 
 usersBtn.addEventListener('click', () => {
-    const id = usersInput.value;
-    const usersUrl = fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const usersUrl = fetch('https://jsonplaceholder.typicode.com/users');
     usersUrl.then((response) => response.json()).then((data) => {
-      const row = tbody.insertRow(-1);
+      data.forEach(element => {
+        const row = tbody.insertRow(-1);
 
-      const cell1 = row.insertCell(0);
-      cell1.innerHTML = data.id;
-      const cell2 = row.insertCell(1);
-      cell2.innerHTML = data.name;
-      const cell3 = row.insertCell(2);
-      cell3.innerHTML = data.username;
-      const cell4 = row.insertCell(3);
-      cell4.innerHTML = data.email;
+        const cell1 = row.insertCell(0);
+        cell1.innerHTML = element.id;
+        const cell2 = row.insertCell(1);
+        cell2.innerHTML = element.name;
+        const cell3 = row.insertCell(2);
+        cell3.innerHTML = element.username;
+        const cell4 = row.insertCell(3);
+        cell4.innerHTML = element.email;
+      });
     })
     document.getElementById('usersTable').style.display = 'table';
 });
