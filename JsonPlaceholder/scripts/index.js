2e588f5312b15b4507f5d34d24a46693214a9b05
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-06-06 10:32:16 -0400

    modified exercise one

--- JsonPlaceholder/scripts/index.js
+++ JsonPlaceholder/scripts/index.js
@@ -1,23 +1,13 @@
 'use strict';
 
-const btn = document.getElementById('btn');
-const tbody = document.querySelector('tbody');
-const todoId = document.getElementById('todoId');
+const goBtnEl = document.getElementById('goBtn');
+const todoInputEl = document.getElementById('todoInput');
+const todoEl = document.getElementById('todo');
 
-btn.addEventListener('click', () => {
-    
-     let fetchAp = fetch('https://jsonplaceholder.typicode.com/todos/1');
-     fetchAp.then(response => response.json()).then(data => {
-        let row = tbody.insertRow(-1);
-        let cell1 = row.insertCell(0);
-        cell1.innerHTML = data.userId;
-        let cell2 = row.insertCell(1);
-        cell2.innerHTML = data.id;
-        let cell3 = row.insertCell(2);
-        cell3.innerHTML = data.title;
-        let cell4 = row.insertCell(3);
-        cell4.innerHTML = data.completed;
-
-     });
-     document.getElementById('messageDiv').style.display = table;
+goBtnEl.addEventListener('click', () => { 
+     const todoId = todoInputEl.value;
+     const url = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
+     fetch(url).then((response) => response.json()).then((todo) => {
+      todoEl.innerHTML = todo.title;
+        })
 });
